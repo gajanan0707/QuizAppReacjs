d6f3f4678ca5ea48c300aebfcd67917c7ce9e1ca
gp <gp.globaliasoft@gmail.com>
2020-12-11 10:27:16 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import Menu from "./components/Menu";
+import Quiz from "./components/Quiz";
+import EndScreen from "./components/EndScreen";
+import { useState } from "react";
+import { GameStateContext } from "./helpers/Contexts";
+// ['menu', 'playing', 'finished']
+function App() {
+  const [gameState, setGameState] = useState("menu");
+  const [userName, setUserName] = useState("");
+  const [score, setScore] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>Quiz App</h1>
+      <GameStateContext.Provider
+        value={{
+          gameState,
+          setGameState,
+          userName,
+          setUserName,
+          score,
+          setScore,
+        }}
+      >
+        {gameState === "menu" && <Menu />}
+        {gameState === "playing" && <Quiz />}
+        {gameState === "finished" && <EndScreen />}
+      </GameStateContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/helpers/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+  {
+    prompt: "What is my name?",
+    optionA: "Robert",
+    optionB: "Captain",
+    optionC: "Thor",
+    optionD: "miss Marvel",
+    asnwer: "optionA",
+  },
+  {
+    prompt: "Which of this is not a programming language?",
+    optionA: "Python",
+    optionB: "JavaScript",
+    optionC: "MC-03",
+    optionD: "Java",
+    asnwer: "optionC",
+  },
+  {
+    prompt: "Which of this is not a javascript framework?",
+    optionA: "React",
+    optionB: "Angular",
+    optionC: "Vue",
+    optionD: "Java",
+    asnwer: "optionD",
+  },
+];
